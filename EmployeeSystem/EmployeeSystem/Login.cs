43ce0fed4cacb2623ea6f0e8cff0f700f01fa53f
newbieUser1809 <148074086+newbieUser1809@users.noreply.github.com>
2023-11-09 08:55:30 +0800

    Add files via upload

---
+++ EmployeeSystem/EmployeeSystem/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeSystem
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUsername.Text) && !string.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Incorrect Password");
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
+        }
+
+
+
+    }
+}
